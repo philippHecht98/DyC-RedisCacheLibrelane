aa8ae4054858d984407aeac046dc980533c4e722
Philipp Hecht <72547449+philippHecht98@users.noreply.github.com>
2026-02-13 10:56:55 +0100

    Feature/controller (#4)
    * added base memory entry of dynamic lenght
    
    * updated dynamic block to use negated reset
    
    * refactored memory_cell module by using registerarray
    
    * finished intial work on memory block
    
    * test for memory_dynamic_registerarray
    
    * created controller
    
    * Update controller for sub fsm layout per op code
    
    * Add cmd structure to sub fsm
    
    ---------
    
    Co-authored-by: Philipp Hecht <philipp.hecht@bmw.de>
    Co-authored-by: Luca Schmid <luca.schmid@hm.edu>
    Co-authored-by: lucaaufgit <not.my@mail.com>

---
+++ src/controller/src/ctrl_types_pkg.sv
@@ -0,0 +1,31 @@
+package ctrl_types_pkg;
+
+    typedef enum logic [2:0] {
+        ST_IDLE,
+        ST_GET,
+        ST_SET,
+        ST_PUT,
+        ST_DEL,
+        ST_ERR
+    } top_state_e;
+
+    typedef enum logic [2:0] {
+        NOOP = 3'b000,
+        READ = 3'b001,
+        CREATE = 3'b010,
+        UPDATE = 3'b011,
+        DELETE = 3'b100
+    } operation_e;
+
+    // Update the substate enums to reflect actual substates for each operation
+    typedef enum logic [1:0] { PUT_ST_START, SOMETHING, ELSE } put_substate_e;
+    typedef enum logic [1:0] { GET_ST_START, SOMETHING, ELSE } get_substate_e;
+    typedef enum logic [1:0] { SET_ST_START, SOMETHING, ELSE } set_substate_e;
+    typedef enum logic [1:0] { DEL_ST_START, SOMETHING, ELSE } del_substate_e;
+
+    typedef struct packed {
+        logic done;
+        logic error;
+    } sub_cmd_t;
+
+endpackage
---
+++ src/memory/src/memory_cell.sv
@@ -0,0 +1,72 @@
+/**
+ * Memory Cell Module
+ * 
+ * Each cell contains:
+ * - Key register (8-bit)
+ * - Value register (64-bit)
+ * - TTL timer (32-bit)
+ */
+
+module memory_cell #(
+    parameter KEY_WIDTH,
+    parameter VALUE_WIDTH
+    //parameter TTL_WIDTH
+)(
+    input wire clk,
+    input wire rst_n,
+    
+    // Write interface
+    input wire write_op,
+    input wire [KEY_WIDTH-1:0] key_in,
+    input wire [VALUE_WIDTH-1:0] value_in,
+    //input wire [TTL_WIDTH-1:0] ttl_in,
+    
+    // Read interface
+    input wire read_op,
+    output reg [KEY_WIDTH-1:0] key_out,
+    output reg [VALUE_WIDTH-1:0] value_out,
+    output reg used_out // Indicates if the cell is currently used (valid)
+    //output reg [TTL_WIDTH-1:0] ttl_out
+);
+    wire in_use;
+
+    // initialize registers by using memory_dynamic_registerarray
+    dynamic_register_array #(.LENGTH(KEY_WIDTH)) key_reg (
+        .clk(clk),
+        .rst_n(rst_n),
+        .write_op(write_op),
+        .select_op(read_op),
+        .data_in(key_in),
+        .data_out(key_out)
+    );
+
+    dynamic_register_array #(.LENGTH(VALUE_WIDTH)) value_reg (
+        .clk(clk),
+        .rst_n(rst_n),
+        .write_op(write_op),
+        .select_op(read_op),
+        .data_in(value_in),
+        .data_out(value_out)
+    );
+
+    dynamic_register_array #(.LENGTH(1)) used_reg (
+        .clk(clk),
+        .rst_n(rst_n),
+        .write_op(write_op),
+        .select_op(read_op),
+        .data_in(in_use),
+        .data_out(used_out) 
+    );
+
+    //dynamic_register_array #(.LENGTH(TTL_WIDTH)) ttl_reg (
+    //    .clk(clk),
+    //    .rst_n(rst_n),
+    //    .write_op(write_op ),
+    //    .select_op(read_op ),
+    //    .data_in(ttl_in),
+    //    .data_out(ttl_out)
+    //);
+
+    assign in_use = (key_out != '0);
+
+endmodule
