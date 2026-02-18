d37adb9dcbd3e57122ade745e95e54ff51927473
Philipp Hecht <philipp.hecht@bmw.de>
2026-02-18 17:24:50 +0000

    updated interface logic to handle read transactions as well

--- src/interface/src/a_channel.sv
+++ src/interface/src/a_channel.sv
@@ -14,13 +14,16 @@ module a_channel #(
     // over the A-channel
     input logic internal_gnt,
 
+     // Signal to indicate if the current operation is a write or read (1=write, 0=read)
+    output logic                  is_write_or_read_operation,
     output logic [ADDR_WIDTH-1:0] addr_out, // Address output to controller
     output logic [DATA_WIDTH-1:0] wdata_out // Write data output to controller
 );
     import if_types_pkg::*;
 
-    reg [ADDR_WIDTH-1:0] addr_reg;
-    reg [DATA_WIDTH-1:0] wdata_reg;
+    reg                     is_write_or_read_operation_reg;
+    reg [ADDR_WIDTH-1:0]    addr_reg;
+    reg [DATA_WIDTH-1:0]    wdata_reg;
 
     // Write logic - A channel
     always_ff @(posedge clk or negedge rst_n) begin
@@ -28,19 +31,28 @@ module a_channel #(
         if (!rst_n) begin
             addr_reg <= '0;
             wdata_reg <= '0;
+            is_write_or_read_operation_reg <= 1'b0;
         end
 
         else if (obi_req.req        // OBI request set to valid from master
                     && obi_req.a.we // Write enable (1=write, 0=read)
-                    && internal_gnt)
-        begin
+                    && internal_gnt) begin
             // Extract lower bits of wdata that match ARCHITECTURE width
             addr_reg <= obi_req.a.addr;
             wdata_reg <= obi_req.a.wdata;
-        end
+            is_write_or_read_operation_reg <= 1'b1;
+        end 
+        else if (obi_req.req 
+                    && !obi_req.a.we // Read enabled (1=write, 0=read)
+                    && internal_gnt) begin
+            addr_reg <= obi_req.a.addr;
+            is_write_or_read_operation_reg <= 1'b0;
+        end 
     end
 
     assign addr_out = addr_reg;
     assign wdata_out = wdata_reg;
+    assign is_write_or_read_operation = is_write_or_read_operation_reg;
+    
 
 endmodule
